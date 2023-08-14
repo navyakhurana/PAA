decf1140bf6161c236cf38343eb423203a339cf0
Navya khurana <navya.khurana@sap.com>
2023-08-14 08:23:42 +0530

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PAA
+# PAA-projects-gitrepos
+
+You can visit the landing page **[here](https://pages.github.tools.sap/platform-adoption-advisory/paa-projects-gitrepos/)**.
---
+++ docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Build resilient apps with SAP BTP',
+  tagline: '',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://github.tools.sap',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/platform-adoption-advisory/paa-projects-gitrepos',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'platform-adoption-advisory', // Usually your GitHub org/user name.
+  projectName: 'paa-projects-gitrepos', // Usually your repo name
+  deploymentBranch: 'paa-doc',
+  trailingSlash: false,
+
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
+
+  scripts: [
+    // ...
+    {
+      src: 'js/custom.js',
+      async: true,
+    },
+  ],
+
+  plugins: [require.resolve("docusaurus-plugin-image-zoom")],
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {routeBasePath: '/',
+
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        // blog: {
+        //   showReadingTime: true,
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      algolia: {
+        apiKey: '745c0c76679d42edf797c5eae4f4b0c8',
+        indexName: 'paa_projects',
+        contextualSearch: true,
+        placeholder: 'Search',
+        appId: 'SX6JA1B3F5',
+        //Search other parameters
+
+      },
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 5,
+      },
+      zoom: {
+        selector: '.markdown :not(em) > img',
+        config: {
+          // image zoom
+          background: {
+            light: 'rgb(255, 255, 255)',
+            dark: 'rgb(50, 50, 50)'
+          }
+        }
+      },
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Develop a multitenant Software as a Service application in SAP BTP using CAP',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/githubFolder.png',
+        },
+        items: [
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/SAP-samples/btp-kyma-cap-multitenant-susaas',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'SAP Community',
+            items: [
+              {
+                label: 'Blogs',
+                href: 'https://blogs.sap.com/',
+              },
+              {
+                label: 'Q&A',
+                href: 'https://answers.sap.com/index.html',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Discovery Center',
+                href: 'https://discovery-center.cloud.sap/index.html',
+              },
+              {
+                label: 'GitHub Repo',
+                href: 'https://github.com/sap-samples',
+              },
+            ],
+          },
+        ],
+        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
+
+
